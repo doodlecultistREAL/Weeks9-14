0f90b007c36d54c8e542cda993965e7d8224b1c5
doodlecultistREAL <juliajulianleyva@gmail.com>
2026-03-18 10:04:22 -0400

    Coroutine

---
+++ Assets/Scripts/growing.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using UnityEngine;
+
+public class growing : MonoBehaviour
+
+
+    
+{
+
+    //came in late, couldn't get it to work. Gonna try again in my own time when the tutorials are out.
+/*
+    public Transform apple;
+    public Transform tree;
+
+
+    Coroutine doTheGrowingCoroutine;
+    Coroutine growTheTreeCoroutine;
+    Coroutine growTheAppleCoroutine;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        tree.localScale = Vector2.zero;
+        apple.localScale = Vector2.zero;
+    }
+
+    public void StartTreeGrowing()
+    {
+        if (doTheGrowingCoroutine != null)
+        {
+            StopCoroutine(DoTheGrowingCoroutine);
+          
+        }
+
+        doTheGrowingCoroutine = StartCoroutine(DoTheGrowing());
+
+
+    }
+
+
+    IEnumerator DoTheGrowing()
+    {
+        yield return growTheAppleCoroutine = StartCoroutine(GrowApple());
+        yield return growTheTreeCoroutine = StartCoroutine(GrowTree());
+      
+    }
+
+    IEnumerable GrowApple()
+    {
+        tree.localScale = Vector2.zero;
+        apple.localScale = Vector2.zero;
+
+    }
+
+    IEnumerator GrowTree()
+    {
+        tree.localScale = Vector2.zero;
+        apple.localScale = Vector2.zero;
+
+    }
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+*/
+}
